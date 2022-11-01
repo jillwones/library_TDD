0adafbc56d228fe81cc550813ee34376f2a5c1fb
williamjones <willj836@gmail.com>
2022-11-01 21:10:18 +0000

    refactored using attr_accessor

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,8 @@
 class Book 
+  attr_accessor :title, :author, :category
   def initialize(title, author, category)
     @title = title 
     @author = author 
     @category = category 
-  end
-  def title 
-    @title
-  end
-  def category 
-    @category
   end
 end
