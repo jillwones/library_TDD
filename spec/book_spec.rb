7dc78e54366bd2d1f6e1a26a53751767fbb36feb
williamjones <willj836@gmail.com>
2022-11-01 20:58:43 +0000

    test written to throw argument error when fewer than 3 args given

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -9,5 +9,9 @@ describe Book do
     it "returns a new book object" do 
       expect(@book).to be_an_instance_of(Book)
     end 
+
+    it "ArgumentError when passed < 3 arguments" do 
+      expect{ Book.new "Title", "Author" }.to raise_error ArgumentError
+    end
   end
 end
