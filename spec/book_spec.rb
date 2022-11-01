4c35cb9f8efab51e76e763d6cea8be4683df45de
williamjones <willj836@gmail.com>
2022-11-01 21:08:20 +0000

    tests added for title and category method

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,13 +5,25 @@ describe Book do
     @book = Book.new 'Title', 'Author', :category
   end 
 
-  describe "#new" do 
-    it "returns a new book object" do 
+  describe '#new' do 
+    it 'returns a new book object' do 
       expect(@book).to be_an_instance_of(Book)
     end 
 
-    it "ArgumentError when passed < 3 arguments" do 
-      expect{ Book.new "Title", "Author" }.to raise_error ArgumentError
+    it 'ArgumentError when passed < 3 arguments' do 
+      expect{ Book.new 'Title', 'Author' }.to raise_error ArgumentError
+    end
+  end
+
+  describe '#title' do 
+    it 'returns the correct title' do 
+      expect(@book.title).to eq('Title')
+    end
+  end
+
+  describe '#category' do 
+    it 'returns the correct category' do 
+      expect(@book.category).to eq(:category)
     end
   end
 end
