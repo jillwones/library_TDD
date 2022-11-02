6f367ab7488a07522a2b723d8279bd347f85f6d2
williamjones <willj836@gmail.com>
2022-11-02 11:00:07 +0000

    methods added for accepting books and retrieving books that satisfy tests

--- lib/library.rb
+++ lib/library.rb
@@ -8,4 +8,17 @@ class Library
     @books = @lib_file ? YAML.load(File.read(@lib_file)) : []
   end
 
+  def get_books_in_cat(category)
+    @books.select do |book|
+      book.category == category 
+    end
+  end
+
+  def add_book(book)
+    @books << book
+  end
+
+  def get_book(book_title)
+    @books.select { |book| book.title == book_title}.first
+  end
 end
